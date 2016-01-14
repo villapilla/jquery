a5c449af594e69b01edb905d7045e065461ea7c0
villapilla <dvillaluenga@gmail.com>
2016-01-14 19:59:44 +0100

    empezamos con jquery

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$('document').ready(function() {
+	$('*').addClass('reset');
+	$('nav ul').addClass('navigation_bar');
+	$('nav ul li').addClass('menu');
+	$('nav + section').addClass('content');
+	$('nav + section li').addClass('product');
+	$('nav + section h2').addClass('product_title');
+	$('nav + section figure img').addClass('picture');
+})
